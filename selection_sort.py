c9354005e6284d6a3420866f0672b5a4f6b4dd47
Alex-Budaghyan <135352716+Alex-Budaghyan@users.noreply.github.com>
2023-07-13 22:49:55 +0400

    selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,10 +1,20 @@
 def selection_sort(array: list) -> list:
-    for i in range(len(array)):
-        small = min(array[i:])
-        index_small = array.index(small)
-        array[i], array[index_small] = array[index_small], array[i]
+    try:
+        array = list(array)
+    except TypeError:
+        raise TypeError("Array is not a list and can't be casted to list")
+    try:
+        for i in range(len(array)):
+            min_index = array.index(min(array[i:]))
+            array[i], array[min_index] = array[min_index], array[i]
+    except TypeError:
+        raise TypeError("The types are incomparable")
     return array
 
 
-arr = [1, 5, 7, 8, 3, 4, 9, 6, 2]
-print(selection_sort(arr))
+ls1 = [1, 5, 7, 9, 3, 4, 2, 8]
+
+try:
+    print(selection_sort(ls1))
+except TypeError as t:
+    print(str(t))
