3011607952930084cffb1ba2e700b41bcbf34424
Alex-Budaghyan <135352716+Alex-Budaghyan@users.noreply.github.com>
2023-07-13 22:46:59 +0400

    biary_search.py

--- biary_search.py
+++ biary_search.py
@@ -1,20 +1,34 @@
+class TargetNotFoundException(Exception):
+    pass
+
+
 def binary_search(ls, target):
+    try:
+        target = float(target)
+    except ValueError:
+        raise ValueError("Target is not a valid number")
+
     low = 0
     high = len(ls) - 1
-    while low <= high:
-        mid = (low + high) // 2
-        if ls[mid] == target:
-            return mid
-        if ls[mid] < target:
-            low = mid + 1
-        else:
-            high = mid - 1
-    return -1
+    try:
+        while low <= high:
+            mid = (low + high) // 2
+            if target > ls[mid]:
+                low = mid + 1
+            elif target < ls[mid]:
+                high = mid - 1
+            else:
+                return mid
+        raise TargetNotFoundException("Target not found in the list")
+
+    except Exception:
+        raise TargetNotFoundException("Target not found in the list")
 
 
-ls1 = [1, 2, 8, 9, 15, 54, 96, 102]
-index = binary_search(ls1, 96)
-if index == -1:
-    print("target not found")
-else:
-    print(index)
+list1 = [1, 2, 4, 5, 9, 45, 155]
+try:
+    print(binary_search(list1, "s"))
+except ValueError as ve:
+    print(str(ve))
+except TargetNotFoundException as tnf:
+    print(str(tnf))
