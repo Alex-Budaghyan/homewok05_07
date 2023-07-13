691248e5233d213e4fbc299cd858e47f60a24e2a
Alex-Budaghyan <135352716+Alex-Budaghyan@users.noreply.github.com>
2023-07-13 22:49:35 +0400

    insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,15 +1,27 @@
 def insertion_sort(array: list) -> list:
-    for i in range(1, len(array)):
-        key = array[i]
-        j = i - 1
-        while j >= 0 and array[j] > key:
-            array[j + 1] = array[j]
-            j -= 1
-        array[j+1] = key
+    try:
+        array = list(array)
+    except TypeError:
+        raise TypeError("Array is not a list and can't be casted to list")
+
+    index = 0
+    while index < len(array) - 1:
+        try:
+            i = index
+            while array[i] > array[i + 1] and i >= 0:
+                array[i], array[i + 1] = array[i + 1], array[i]
+                i -= 1
+            index += 1
+        except TypeError:
+            raise TypeError("The types are incomparable")
     return array
 
 
-arr1 = [1, 5, 6, 4, 7, 3, 9, 8, 2]
-print(insertion_sort(arr1))
+ls1 = [1, 5, 7, 9, 3, 4, 2, 8]
 
+
+try:
+    print(insertion_sort(ls1))
+except TypeError as t:
+    print(str(t))
 
