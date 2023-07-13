95f4cd78bf4fdd2bef1094f499b1b9ddb705e3bb
Alex-Budaghyan <135352716+Alex-Budaghyan@users.noreply.github.com>
2023-07-13 22:46:34 +0400

    Sum_of digits.py

--- Sum_of digits.py
+++ Sum_of digits.py
@@ -1,8 +1,14 @@
 def sum_of_digits(num):
+    if type(num) is not int:
+        raise ValueError("Num must be integer")
+
     if num < 10:
         return num
-    return num % 10 + sum_of_digits(num // 10)
+    else:
+        return num % 10 + sum_of_digits(num // 10)
 
 
-print(sum_of_digits(158))
-print(sum_of_digits(147852))
+try:
+    print(sum_of_digits(178561))
+except ValueError as ve:
+    print(str(ve))
